0c458c45bc056e871b3e135f41bb4ac6255108b3
michael-epn <michael.vargas@epn.edu.ec>
2026-05-28 01:11:35 -0500

    server cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,22 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import cors from 'cors'
+import cors from 'cors' // Solo una vez
 import estudianteRoutes from './routers/estudiante_routes.js'
 import docenteRoutes from './routers/docente_routes.js'
 import direccionRoutes from './routers/direccion_routes.js'
-import cors from 'cors'
 
 const app = express()
 dotenv.config()
 
 app.set('port', process.env.PORT || 3000)
 
+const corsOptions = {
+    origin: process.env.URL_FRONTEND, 
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions)) // Se aplica una sola vez con las opciones correctas
 app.use(express.json())
-app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("Sistema Inteligente de Recomendacion de Tesis")
@@ -21,13 +25,6 @@ app.get('/', (req, res) => {
 app.use('/api/estudiante', estudianteRoutes)
 app.use('/api/docente', docenteRoutes)
 app.use('/api/direccion', direccionRoutes)
-
-const corsOptions = {
-    origin: process.env.URL_FRONTEND,
-    optionsSuccessStatus: 200
-}
-
-app.use(cors(corsOptions))
 
 app.use((req, res) => res.status(404).send("Endpoint no encontrado - 404"))
 
