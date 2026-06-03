1b69fa6c05c01784c5b9148ed60ccc91ba2b412b
michael <michael.vargas@epn.edu.ec>
2026-06-03 19:11:49 +0000

    cambio en el main frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import ThemeProvider from './utils/ThemeContext'
 import App from './App'
 import { ToastContainer } from 'react-toastify'
@@ -9,11 +9,11 @@ import './css/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <ThemeProvider>
         <App />
         <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
       </ThemeProvider>
-    </HashRouter>
+    <BrowserRouter/>
   </React.StrictMode>
 )
