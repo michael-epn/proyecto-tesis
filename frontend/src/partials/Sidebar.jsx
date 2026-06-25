cd554a38f67ab4d3e8412287aeb2d51e61207a7f
michael <michael.vargas@epn.edu.ec>
2026-06-25 09:51:22 -0500

    quite una s jajajaja

--- frontend/src/partials/Sidebar.jsx
+++ frontend/src/partials/Sidebar.jsx
@@ -90,7 +90,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${pathname === '/estudiante' && 'bg-slate-900'}`}>
                     <NavLink end to="/estudiante" className={`block text-slate-200 truncate transition duration-150 ${pathname === '/estudiante' ? 'hover:text-slate-200' : 'hover:text-white'}`}>
                       <div className="flex items-center">
-                        <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 duration-200">Generar Temas</span>
+                        <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 duration-200">Generar Tema</span>
                       </div>
                     </NavLink>
                   </li>
